3f747988a0bc446bef2b684dd71bab613e106058
Stephen Harrison <jake.harrison@gmail.com>
2021-02-10 12:07:38 -0500

    Add files via upload

---
+++ WOL/TimeoutBeforeSleep.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = "SilentlyContinue"
+$SecondsToWait = 1200
+$strQuery = "Select InstanceID from Win32_PowerPlan where IsActive = 'true'"
+$PowerPlanInstanceID = (Get-WmiObject -Query $strQuery -Namespace root\cimv2\power).InstanceID.ToString()
+$RegEx = [RegEx]"{(.*?)}$"
+$GUID = $RegEx.Match($PowerPlanInstanceID).Groups[1].Value
+New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\7bc4a2f9-d8fc-4469-b07b-33eb785aaca0\DefaultPowerSchemeValues\$GUID"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\7bc4a2f9-d8fc-4469-b07b-33eb785aaca0\DefaultPowerSchemeValues\$GUID" -Name "AcSettingIndex" -Value "$SecondsToWait" -Type "DWORD" -Force
---
+++ WOL/WOLfromSleep.ps1
@@ -0,0 +1,12 @@
+﻿$PNPCapabilities = 0
+$Index = $null
+$query = "select * from Win32_NetworkAdapterConfiguration Where IPEnabled = 'True'"
+Get-WmiObject -Query $query | ForEach-Object {
+    $Index = $_.Index
+	if ($Index.Length -ne 4) {
+		do {
+			$Index = "0$Index"
+		} while ($Index.Length -lt 4)
+	}
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\$Index" -Name "PNPCapabilities" -Value "$PNPCapabilities" -Type "DWORD" -Force
+}
