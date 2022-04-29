1c61bc09828ce4b4786d955799d285762dab2f60
Stephen Harrison <jake.harrison@gmail.com>
2022-04-29 10:23:05 -0400

    Add files via upload

---
+++ ImportSCUPcert.ps1
@@ -0,0 +1,7 @@
+﻿$registryPath = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate"
+$Name = "AcceptTrustedPublisherCerts"
+$value = "1"
+
+New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher -FilePath SCUP.cer
+Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath SCUP.cer
