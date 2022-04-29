5b4dc748f15c73475adc44b32a957d55bf438c6c
Stephen Harrison <jake.harrison@gmail.com>
2022-04-29 10:05:53 -0400

    Add files via upload

---
+++ GuestUsers_AcctCreation_Clean.ps1
@@ -0,0 +1,14 @@
+﻿$AcctNumber = 00
+$Password = 'VerySecurePlainTextPassword'
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+$Path = 'OU=Site1,OU=Guests,DC=dc2,DC=dc1,DC=com'
+$Server = 'dc2.dc1.com'
+
+while($AcctNumber -lt 50){
+
+    $AcctNumber = (1 + $AcctNumber).ToString('00')
+    $Name = 'Guests'+$AcctNumber
+    New-ADUser -Name $Name -CannotChangePassword $true -ChangePasswordAtLogon $false -AccountPassword $SecurePassword -Enabled $true -OtherAttributes @{'givenName'='Guest';'displayName'='Guest';'userPrincipalName'=$Name+'@dc2.dc1.com'} -Path $Path -Server $Server
+}
+
