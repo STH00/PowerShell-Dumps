17268ebda54baf3dee94ea2a46ca005af6b56f64
Stephen Harrison <jake.harrison@gmail.com>
2022-04-29 09:11:42 -0400

    Add files via upload

---
+++ AD_Disable_and_Move_Clean.ps1
@@ -0,0 +1,10 @@
+﻿$Users = Get-Content "C:\Path\disable.csv"
+$OU = "OU=Disabled,OU=Users,DC=Domain2,DC=Domain,DC=com"
+$Server = "ad.server.com"
+
+
+ForEach($User in $Users){ 
+    $UserDN = (Get-ADUser -LDAPFilter "(mail=$User)" -Properties *).DistinguishedName
+    Move-ADObject -Identity $UserDN -TargetPath $OU -Server $Server -PassThru | Disable-ADAccount
+    Write-Host "Disabled $UserDN"
+} 
